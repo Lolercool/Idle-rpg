de1472b0c48ffb0334a5fd668e82181a92af8ab6
Lolercool <woldkristian080@gmail.com>
2026-08-12 09:39:07 +0200

    Added a small list of items

---
+++ items.js
@@ -0,0 +1,99 @@
+let items = [
+    {
+        id: "wheat",
+        name: "Wheat",
+        type: "resource",
+        category: "farming",
+        rarity: "common",
+        stackable: true,
+    },
+    {
+        id: "carrot",
+        name: "Carrot",
+        type: "resource",
+        category: "farming",
+        rarity: "common",
+        stackable: true,
+    },
+    {
+        id: "iron_ore",
+        name: "Iron Ore",
+        type: "resource",
+        category: "mining",
+        rarity: "common",
+        stackable: true,
+    },
+    {
+        id: "gold_ore",
+        name: "Gold Ore",
+        type: "resource",
+        category: "mining",
+        rarity: "common",
+        stackable: true,
+    },
+    {
+        id: "oak_log",
+        name: "Oak Log",
+        type: "resource",
+        category: "foraging",
+        rarity: "common",
+        stackable: true,
+    },
+    {
+        id: "goblin_ear",
+        name: "Goblin Ear",
+        type: "resource",
+        category: "combat",
+        rarity: "common",
+        stackable: true,
+    },
+
+    {
+        id: "iron_sword",
+        name: "Iron Sword",
+        type: "sword",
+        category: "weapons",
+        rarity: "common",
+        stackable: false
+    },
+    {
+        id: "steel_sword",
+        name: "Steel Sword",
+        type: "sword",
+        category: "weapons",
+        rarity: "uncommon",
+        stackable: false
+    },
+    {
+        id: "iron_helmet",
+        name: "Iron Helmet",
+        type: "helmet",
+        category: "armor",
+        rarity: "common",
+        stackable: false
+    },
+    {
+        id: "explorer_boots",
+        name: "Explorer Boots",
+        type: "boots",
+        category: "armor",
+        rarity: "uncommon",
+        stackable: false
+    },
+    {
+        id: "iron_pickaxe",
+        name: "Iron Pickaxe",
+        type: "pickaxe",
+        category: "tools",
+        rarity: "common",
+        stackable: false
+    },
+    {
+        id: "steel_axe",
+        name: "Steel Axe",
+        type: "axe",
+        category: "tools",
+        rarity: "uncommon",
+        stackable: false
+    },
+];
